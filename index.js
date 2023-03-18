45c687beaa755300aaf373b70946d0e0ac058066
Michael <michaellimisi7@gmail.com>
2023-03-18 10:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNum) {
+
+if (someNum === 199 || someNum=== 400){
+    
+  return 'This one is on me!'
+
+  }else if (someNum === 2000 || someNum=== 2001){
+
+     return 'I will gladly take your thirty bucks.'
+
+  }else if (someNum === 2500 || someNum=== 2501) {
+    
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+  if (city === 'NYC'){
+    return ('Ok, sounds good.')
+  }else if (city != 'NYC') {
+    return ('No go.')
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+// function switchOnCharmFromTip()
+
+function switchOnCharmFromTip(tip) {
+
+  // switch (tip) {
+  //   case 'generous': 'Thank you so much.'
+  //   break;
+  //   case 'not as generous' : 'Thank you.'
+  //   break;
+  //   default: 'Bye.'
+  //   break;
+  // }
+  if (tip === 'generous'){
+    return ('Thank you so much.')
+  }else if (tip === 'not as generous') {
+    return ('Thank you.')
+  }else if (tip === 'thanks for everything') {
+    return ('Bye.')
+  }
 }
